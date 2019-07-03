4790a341767e0cd839b446662078aeb15152d452
Sushen Biswas <sushenbiswasaga@gmail.com>
2019-07-03 09:56:23 +0600

    Random Number Generate Just Fine.

--- src/NumberGames.java
+++ src/NumberGames.java
@@ -1,2 +1,13 @@
+
+
+import java.util.Scanner;
+
 public class NumberGames {
+
+    public static void main(String [] args){
+        int randomNumber = (int) (Math.random() * 100) + 1;
+        System.out.println("I have randomly choosen a number between 1 and 100");
+        System.out.println("Try To Guess It");
+        System.out.println(randomNumber);
+    }
 }
