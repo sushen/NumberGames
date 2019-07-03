0fce139675042ff58669cebbdf5062b6f99b1b1d
Sushen Biswas <sushenbiswasaga@gmail.com>
2019-07-03 10:00:08 +0600

    Guess Loop Working.

--- src/NumberGames.java
+++ src/NumberGames.java
@@ -8,6 +8,9 @@ public class NumberGames {
         int randomNumber = (int) (Math.random() * 100) + 1;
         System.out.println("I have randomly choosen a number between 1 and 100");
         System.out.println("Try To Guess It");
-        System.out.println(randomNumber);
+
+        for (int i = 10; i > 0 ; i--){
+            System.out.println("You Have " + i + " Guess Left");
+        }
     }
 }
