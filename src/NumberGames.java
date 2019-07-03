a6431527c1a78fe027f676e1f4ae9465bff4d71a
Sushen Biswas <sushenbiswasaga@gmail.com>
2019-07-03 10:46:04 +0600

    Logical Flow Working. Now it a playable games.

--- src/NumberGames.java
+++ src/NumberGames.java
@@ -6,11 +6,32 @@ public class NumberGames {
 
     public static void main(String [] args){
         int randomNumber = (int) (Math.random() * 100) + 1;
+        boolean hasWon = false;
+
+
         System.out.println("I have randomly choosen a number between 1 and 100");
         System.out.println("Try To Guess It");
 
+        Scanner scanner = new Scanner(System.in);
+
         for (int i = 10; i > 0 ; i--){
-            System.out.println("You Have " + i + " Guess Left");
+            System.out.println("You Have " + i + " Guess Left. Choose Again");
+            int guess = scanner.nextInt();
+
+            if (randomNumber < guess){
+                System.out.println("Its smaller then " + guess + "Guess");
+            }
+            if (randomNumber > guess){
+                System.out.println("Its larger then " + guess + "Guess");
+            }
+            if (randomNumber == guess){
+                hasWon = true;
+                break;
+            }
+
+        }
+        if (hasWon){
+            System.out.println("CORRECT ...... You Won!!");
         }
     }
 }
