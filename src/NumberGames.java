d094c98b6d10aaa3da92ea30595cd80e41131681
Sushen Biswas <sushenbiswasaga@gmail.com>
2019-07-31 10:24:16 +0600

    What if they insert string.

--- src/NumberGames.java
+++ src/NumberGames.java
@@ -1,41 +1,49 @@
-
+//Input The Scanner class
 
 import java.util.Scanner;
 
-public class NumberGames {
+// Java Class
+public class NumberGames{
+//  Main Method
+    public static void main(String args[]){
 
-    public static void main(String [] args){
-        int randomNumber = (int) (Math.random() * 100) + 1;
+        System.out.println("This is a  Number Gassing Games!! ");
+        System.out.println("You have to guess a number between 1 to 100");
+
+//      Generate a random number between 1-100.
+        int rangomNumber = (int) (Math.random()*100) +1;
         boolean hasWon = false;
 
 
-        System.out.println("I have randomly choosen a number between 1 and 100");
-        System.out.println("Try To Guess It");
-
+//      Test The Scanner library
         Scanner scanner = new Scanner(System.in);
 
-        for (int i = 10; i > 0 ; i--){
-            System.out.println("You Have " + i + " Guess Left. Choose Again");
-            int guess = scanner.nextInt();
+//      Create a loop that asks the user to guess a number.
+        for (int i = 10; i > 0; i--) {
+//              Read the user's input and compare it to the random number.
+            System.out.println("You have " + i + " guess left." + " Enter The Number:");
+            int gussNumber = scanner.nextInt();
+            System.out.println("Your guess is " + gussNumber);
 
-            // Random Number = 15. Gess = 1, then one of the three will be true
-            if (randomNumber < guess){
-                System.out.println("Its smaller then " + guess + "Guess");
-            }
-            else if (randomNumber > guess){
-                System.out.println("Its larger then " + guess + "Guess");
-            }
-            else {
+//              Let the user know if the guessed number was greater than or less than the random number.
+            if (gussNumber > rangomNumber) {
+                System.out.println("Winning Number is Smaller then your gess ");
+            } else if (gussNumber < rangomNumber) {
+                System.out.println("Winning Number is bigger than your gess. ");
+            } else if (gussNumber == rangomNumber) {
                 hasWon = true;
                 break;
             }
+        }
+
+    //        If they guessed the number right end the loop and tell them they've won.
+        if (hasWon){
+            System.out.println("You win");
+        }else {
+            System.out.println("You Loss, The number you try to find is "+ rangomNumber);
 
         }
-        if (hasWon){
-            System.out.println("CORRECT ...... You Won!!");
-        }else {
-            System.out.println(" Game Over ...... You Lose!!");
-            System.out.println(" The Random Number was " + randomNumber);
-        }
+    //        If they used up all 10 guesses end the loop and tell them they've lost.
+
     }
 }
